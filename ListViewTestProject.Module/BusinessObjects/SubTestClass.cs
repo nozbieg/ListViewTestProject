2f6894613771f17b227bbf793431708f864e74c5
Norbert Olesiak-Zbieg <nolesiak@gabos.pl>
2021-01-12 15:52:03 +0100

    Add project files.

---
+++ ListViewTestProject.Module/BusinessObjects/SubTestClass.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewTestProject.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [XafDefaultProperty(nameof(SubTestName))]
+    public class SubTestClass : XPObject
+    {
+        public SubTestClass(Session session) : base(session)
+        { }
+
+
+        string subTestName;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string SubTestName
+        {
+            get => subTestName;
+            set => SetPropertyValue(nameof(SubTestName), ref subTestName, value);
+        }
+
+        [VisibleInDashboards(false)]
+        [VisibleInDetailView(false)]
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        [Association("SubTest-Testy")]
+        public XPCollection<TestClass> Testy
+        {
+            get
+            {
+                return GetCollection<TestClass>(nameof(Testy));
+            }
+        }
+    }
+}
---
+++ ListViewTestProject.Module/BusinessObjects/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewTestProject.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [XafDefaultProperty(nameof(Name))]
+    public class TestClass : XPObject
+    {
+        public TestClass(Session session) : base(session)
+        { }
+
+
+        string name;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+
+        [Association("SubTest-Testy")]
+        public XPCollection<SubTestClass> SubTesty
+        {
+            get
+            {
+                return GetCollection<SubTestClass>(nameof(SubTesty));
+            }
+        }
+    }
+}
